ea79abdae4dbffdda5d6336f410b29ddf315e88d
Corey Schulz <realcoreyschulz@gmail.com>
2019-12-05 00:18:25 -0700

    Add solution for day3 part 2.

--- day3.py
+++ day3.py
@@ -2,12 +2,17 @@ import sys
 from day3_data import l1Data
 from day3_data import l2Data
 
-line1Points = set()
-line2Points = set()
+line1Points = {}
+line2Points = {}
+
+def addPointToDict(dictionary, x, y, numSteps):
+    if (x, y) not in dictionary.keys():
+        dictionary[(x, y)] = numSteps
 
 def calculatePointsVisited(data, pointSet):
     x = 0
     y = 0
+    numSteps = 0 
 
     for dataPoint in data:
         direction = dataPoint[0]
@@ -15,23 +20,27 @@ def calculatePointsVisited(data, pointSet):
         
         if direction == "L":
             for i in range(amount):
+                numSteps += 1
                 x -= 1
-                pointSet.add((x, y))
+                addPointToDict(pointSet, x, y, numSteps)
                 
         elif direction == "R":
             for i in range(amount):
+                numSteps += 1
                 x += 1
-                pointSet.add((x, y)) 
+                addPointToDict(pointSet, x, y, numSteps)
 
         elif direction == "D":
             for i in range(amount):
+                numSteps += 1
                 y -= 1
-                pointSet.add((x, y))
+                addPointToDict(pointSet, x, y, numSteps)
 
         elif direction == "U":
             for i in range(amount):
+                numSteps += 1
                 y += 1
-                pointSet.add((x, y))
+                addPointToDict(pointSet, x, y, numSteps)
                 
 
 calculatePointsVisited(l1Data, line1Points)
@@ -39,11 +48,25 @@ calculatePointsVisited(l2Data, line2Points)
 
 minDistance = sys.maxsize
 
-for point in line1Points:
-    if point in line2Points:
+## Solve part 1:
+
+for point in line1Points.keys():
+    if point in line2Points.keys():
        tempDist = abs(point[1]) + abs(point[0])
        if tempDist < minDistance:
            minDistance = tempDist
 
 print(minDistance)
+
+minTotalSteps = sys.maxsize 
+
+## Solve part 2:
+for point in line1Points.keys():
+    if point in line2Points.keys():
+        tempHops = line1Points[point] + line2Points[point]
+        if tempHops < minTotalSteps:
+            minTotalSteps = tempHops
+
+print(minTotalSteps) 
+        
 
